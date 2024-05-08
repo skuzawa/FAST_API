08dc4c1ab935cfa20d1e3a471858c81ceafe52a6
Shunsuke Kuzawa <98546409+skuzawa@users.noreply.github.com>
2024-05-08 14:47:37 +0900

    Update route_auth.py

--- routers/route_auth.py
+++ routers/route_auth.py
@@ -36,17 +36,17 @@ async def login(request:Request, response:Response, user:UserBody,csrf_protect:C
     response.set_cookie(
         key="access_token",value=f"Bearer {token}", httponly=True, samesite="none",secure=True
     )
-    return {"message":"Successfully logged-in"}
+    return {"message": "Successfully logged-in"}
 
 @router.post("/api/logout",response_model=SuccessMsg)
 def logout(request:Request,response:Response,csrf_protect:CsrfProtect=Depends()):
     csrf_token = csrf_protect.get_csrf_from_headers(request.headers)
     csrf_protect.validate_csrf(csrf_token)
 
-@router.get('/api/user', response_model='UserInfo')
+@router.get('/api/user', response_model=UserInfo)
 def get_user_refresh_jwt(request:Request,response:Response):
     new_token , subject = auth.verify_update_jwt(request)
     response.set_cookie(
         key="access_token",value=f"Bearer {new_token}", httponly=True, samesite="none",secure=True
     )
-    return {"email",subject}
+    return {"email": subject}
